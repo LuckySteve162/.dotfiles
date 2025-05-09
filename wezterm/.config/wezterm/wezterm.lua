e695bc5e2bda9b2ac83e3634d5920bbe97988181
LuckySteve162 <johnlacy162@gmail.com>
2025-05-09 11:47:32 -0500

    changing color

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -6,7 +6,7 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
-config.color_scheme = 'Batman'
+config.color_scheme = 'Calamity'
 
 -- and finally, return the configuration to wezterm
 return config
